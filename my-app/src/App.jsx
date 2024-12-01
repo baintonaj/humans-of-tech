1d2da1bf17bddb764f04b31a2fcd55cfd090df3c
Andy Bainton <60830258+baintonaj@users.noreply.github.com>
2024-11-30 18:17:27 -0800

    UX+UI and Search enhancements made.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,7 +8,7 @@ import Personpage from './components/Pages/Personpage';
 import {SearchName} from "./components/Pages/SearchName";
 import SignIn from './components/SignIn';
 import About from './components/Pages/About';
-
+import Footer from "./components/Pages/Footer";
 
 const App = () => {
     const KEY = "demo";
@@ -22,7 +22,8 @@ const App = () => {
           <Route path="/personpage" element={<Personpage apiKey={KEY} />} />
             <Route path="/SearchName" element={<SearchName apiKey={KEY} />} />
             <Route path="/SignIn" element={<SignIn />} />
-            <Route path="/SearchName" element={<SearchName apiKey={KEY} />} /> 
+            <Route path="/SearchName" element={<SearchName apiKey={KEY} />} />
+            <Route path="/Footer" element={<Footer />} />
             <Route path="/About" element={<About />} />
         </Routes>
       </main>
--- my-app/src/components/SignIn.jsx
+++ my-app/src/components/SignIn.jsx
@@ -4,39 +4,13 @@ import './SignIn.css';
 import LoginFormFile from "./LoginFormFile";
 
 const SignIn = ({ onSubmit }) => {
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  //
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   onSubmit({ email, password });
-  // };
 
   return (
     <div className="sign-in-container">
-      {/*<form onSubmit={handleSubmit}>*/}
         <h2>Sign In</h2>
-        {/*<label htmlFor="email">Email:</label>*/}
-        {/*<input*/}
-        {/*  type="email"*/}
-        {/*  id="email"*/}
-        {/*  value={email}*/}
-        {/*  onChange={(e) => setEmail(e.target.value)}*/}
-        {/*  required*/}
-        {/*/>*/}
-        {/*<label htmlFor="password">Password:</label>*/}
-        {/*<input*/}
-        {/*  type="password"*/}
-        {/*  id="password"*/}
-        {/*  value={password}*/}
-        {/*  onChange={(e) => setPassword(e.target.value)}*/}
-        {/*  required*/}
-        {/*/>*/}
         <div>
           <LoginFormFile />
         </div>
-        {/*<button type="submit">Sign In</button>*/}
-      {/*</form>*/}
     </div>
   );
 };
